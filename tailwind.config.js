7c0840dbffaba846dcdeab92efe47d9a416da5f8
Dev37 <firstprogram7@gmail.com>
2026-04-09 13:35:17 +0530

    Replaced the index.html to root directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.html"],
+  content: ["./index.html","./src/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
