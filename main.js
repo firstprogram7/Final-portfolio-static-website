38b054f2530ee68484598df7e63a67516d68aa33
Dev37 <firstprogram7@gmail.com>
2026-04-11 02:42:33 +0530

    Second major changes after launch 1. Done face lift of the home page 2. Added about me pages 3. Made dark mode functional 4. Made multiple adjustments in blog pages

--- main.js
+++ main.js
@@ -32,6 +32,55 @@ window.addEventListener("resize", () => {
   }
 });
 
+// Navbar background color change
+window.addEventListener("scroll", () => {
+  const navHeader = document.getElementById("main-header");
+  const logo = document.getElementById("logo");
+  const navLinks = document.querySelectorAll("#nav-links a");
+
+  if (window.scrollY > 50) {
+    // 1. Change background color
+    navHeader.classList.add(
+      "bg-white",
+      "dark:bg-[#121212]",
+      "transition-all",
+      "duration-150",
+      "shadow-md",
+      "z-50"
+    );   
+    // 2. Shrink logo
+    logo.classList.replace("md:text-5xl", "text-2xl");
+    // 3. Shring nav links to text-sm
+    navLinks.forEach((link) => {
+      link.classList.remove("md:text-xl", "lg:text-2xl");
+      link.classList.add("text-sm");
+    });
+  } else {
+    // Reset everything when back at the top
+    navHeader.classList.remove("bg-white", "shadow-md", "dark:bg-[#121212]");
+    logo.classList.replace("text-2xl", "md:text-5xl");
+
+    navLinks.forEach((link) => {
+      link.classList.add("md:text-xl", "lg:text-2xl");
+      link.classList.remove("text-sm");
+    });
+  }
+});
+
+// Dark mode toggle button
+const html = document.documentElement;
+const lightBtn = document.getElementById("light-mode");
+const darkBtn = document.getElementById("dark-mode");
+lightBtn.addEventListener("click", () => {
+  html.classList.remove("dark");
+  localStorage.setItem("theme","light");
+});
+darkBtn.addEventListener("click", () => {
+  html.classList.add("dark");
+  localStorage.setItem("theme","dark");
+});
+
+
 // Card stack function
 let lastScrollTop = 0;
 
@@ -86,4 +135,4 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// Scroll indicator
+// Scroll Reveal about page
